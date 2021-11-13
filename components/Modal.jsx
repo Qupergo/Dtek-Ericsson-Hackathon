2c881ae0bab62a9af31471924670684f71e2749c
Ryan Jin <ryanjin007@gmail.com>
2021-11-13 23:16:29 +0100

    Update Modal.jsx

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -3,23 +3,66 @@ import Popup from 'reactjs-popup';
 import styled from 'styled-components'
 
 const Modal = () => { 
-  const [table, setTable] = useState({});
 
+  const [getTitle, setGetTitle] = useState(true)
+
+  const [title, setTitle] = useState()
+
+  const [person, setPerson] = useState({});
+
+  const [persons, setPersons] = useState([]);
+
+  const handleTitleSubmission = () => {
+    setGetTitle(false);
+  }
+
+  const handlePersonSubmission = (event) => {
+      event.preventDefault()
+      setPersons([...persons, person]);
+      setPerson({})
+  }
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handlePersonChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setPerson(values => ({...values, [name]: value}))
+  }
+
+  const handleCancel = (event) => {
+    event.preventDefault()
+    setGetTitle(true)
+    setTitle("")
+    setPerson({})
+    setPersons([])
+  }
 
   return (
     <StyledPopup
     trigger={<button className="button button py-2 px-4 rounded-lg inline-flex flex-row items-center space-x-2 duration-200 text-black hover:bg-secondary"><span className="font-semibold">Create</span></button>}
     modal
-        closeOnDocumentClick
     >
-      <form>
-        <label for="title">Leaderboard name:</label><br />
-        <input type="text" id="title" name="title" /><br />
-        <label for="name">Full name: </label><br />
-        <input type="text" id="fname" name="fname" /><br />
-        <label for="score">Score: </label><br />
-        <input type="text" id="score" name="score" /><br />
-      </form>
+        {getTitle ? 
+          <form onSubmit={getTitle ? handleTitleSubmission : handlePersonSubmission}>
+              <label for="title">Leaderboard name:</label><br />
+              <input type="text" id="title" name="title" onChange={handleTitleChange} value={getTitle ? title : ""}/><br />
+              <button onClick={handleTitleSubmission}>Next</button>
+              <button onClick={handleCancel}>Cancel</button>
+          </form>
+              :
+            <form>
+              <label for="name">Full name: </label><br />
+              <input type="text" id="name" name="name" onChange={handlePersonChange} value={person.name || ""} /><br />
+              <label for="score">Score: </label><br />
+              <input type="text" id="score" name="score" onChange={handlePersonChange} value={person.score || ""}/><br />
+              <button onClick={handlePersonSubmission}>Next</button>
+              {/* <button onClick={}>Done</button> */}
+              <button onClick={handleCancel}>Cancel</button>
+            </form>
+        }
     </StyledPopup>
   )};
 
