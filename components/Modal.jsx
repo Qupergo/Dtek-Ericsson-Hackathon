712f218b1e73d123db34381b66a75822fcda32d4
Qupergo <45945560+Qupergo@users.noreply.github.com>
2021-11-14 12:25:58 +0100

    Update Modal.jsx

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Popup from 'reactjs-popup';
 import styled from 'styled-components'
 import Button from "./Button"
-
+import ButtonGroup from "./ButtonGroup"
 
 const Modal = ({setData}) => { 
 
@@ -62,16 +62,19 @@ const Modal = ({setData}) => {
             <div className="flex flex-row items-center border-b border-primary py-2">
               <label for="title"></label><br />
               <input className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Leaderboard Name" aria-label="Leaderboard Name" type="text" id="title" name="title" onChange={handleTitleChange} value={getTitle ? title : ""}/>
-              <Button type="primary" onClick={handleTitleSubmission}>Next</Button>
-              <Button type="light" onClick={() => {
-                handleCancel();
-                close();
-              }}>Cancel</Button>
+              <ButtonGroup>
+                <Button type="primary" onClick={handleTitleSubmission}>Next</Button>
+                <Button type="light" onClick={() => {
+                  handleCancel();
+                  close();
+                }}>Cancel</Button>
+              </ButtonGroup>
+
             </div>
           </form>
               :
             <form>
-              <div className="">
+              <div>
                 <input className="shadow-md rounded appearance-none bg-gray-100 border-none w-full text-gray-700 mb-2 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Nick" aria-label="Nick" type="text" id="name" name="name" onChange={handlePersonChange} value={person.name || ""} /><br />
                 <input className="shadow-md rounded appearance-none bg-gray-100 border-none w-full text-gray-700 mb-4 mr-3 py-1 px-2 leading-tight focus:outline-none" placeholder="Score" aria-label="Score" type="text" id="score" name="score" onChange={handlePersonChange} value={person.score || ""} /><br />
               </div>
